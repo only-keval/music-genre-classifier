1404b8ed387e033216268e03ba12e2ffa7c1d1f7
Keval Bhavsar <kevalbhavsar1404@gmail.com>
2026-06-15 00:36:14 +0530

    separate predict cli from inference code

--- src/inference.py
+++ src/inference.py
@@ -1,4 +1,3 @@
-import argparse
 from pathlib import Path
 
 import torch
@@ -59,53 +58,3 @@ def predict(
             len(idx_to_genre)
         )
     ]
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="Predict music genre"
-    )
-
-    parser.add_argument(
-        "audio_path",
-        type=Path,
-        help="Path to audio file",
-    )
-
-    args = parser.parse_args()
-
-    device = torch.device(
-        "cuda"
-        if torch.cuda.is_available()
-        else "cpu"
-    )
-
-    model, idx_to_genre = load_model(
-        device
-    )
-
-    predictions = predict(
-        model,
-        idx_to_genre,
-        args.audio_path,
-        device,
-    )
-
-    predictions.sort(
-        key=lambda x: x[1],
-        reverse=True,
-    )
-
-    print()
-    print("Predictions:")
-    print()
-
-    for genre, prob in predictions:
-        print(
-            f"{genre:<12}"
-            f"{prob * 100:.2f}%"
-        )
-
-
-if __name__ == "__main__":
-    main()
